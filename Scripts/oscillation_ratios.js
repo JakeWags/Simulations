45de86c1f068d41a4c245912288efab2b20397c9
Jake Wags <jakewagoneredu@gmail.com>
2022-05-27 17:24:36 -0600

    Stuff

---
+++ Scripts/oscillation_ratios.js
@@ -0,0 +1,90 @@
+window.onload = function() {
+	let canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
+	const width = canvas.width = window.innerWidth;
+	const height = canvas.height = window.innerHeight;
+
+	let centerY = height * 0.5,
+	    centerX = width * 0.5,
+	    baseRadius = 5,
+	    xRadius = 200,
+	    yRadius = 200,
+	    xAngle = 0.01,
+	    yAngle = 0.01,
+	    xSpeed = 0.02,
+	    ySpeed = 0.02,
+	    r = 0,
+	    g = 0,
+	    b = 0;
+
+	let counter = 0;
+
+	initText();
+
+	function initText() {
+		updateTitleText();
+		updateRatioText();
+	}
+
+	function updateTitleText() {
+		ctx.font = "40px Arial";
+		ctx.textAlign = "center";
+		ctx.fillText("Harmonic Oscillator Ratios", width/2, 100);
+		ctx.font = "25px Arial";
+		ctx.fillText("UP/DOWN: increase/decrease y oscillations.", width/2, 150);
+		ctx.fillText("RIGHT/LEFT: increase/decrease x oscillations.", width/2, 180);
+	}
+
+	function updateRatioText() {
+		ctx.fillStyle = "rgb(0,0,0)";
+		ctx.font = "40px Arial";
+		ctx.fillText(`X: ${Math.round(xSpeed*100)}, Y: ${Math.round(ySpeed*100)}`, 200, 100);
+	}
+
+	render();
+
+	document.onkeydown = getKey;
+
+	function getKey(e) {
+		switch (e.code) {
+			case "ArrowUp":
+				ySpeed += 0.01;
+				break;
+			case "ArrowDown":
+				ySpeed += -0.01;		
+				break; 
+			case "ArrowRight":
+				xSpeed += 0.01;
+				break;
+			case "ArrowLeft":
+				xSpeed += -0.01;
+				break;
+			default:
+				return;
+		}
+
+		yAngle = 0;
+		xAngle = 0;
+		ctx.clearRect(0,0,width,height);
+		initText();
+		updateRatioText();
+	}
+
+	function render() {
+		counter++;
+		let x = centerX + Math.cos(xAngle) * xRadius;
+		let y = centerY + Math.sin(yAngle) * yRadius;
+		
+		ctx.fillStyle = `rgb(${r},${g},${b})`;
+
+		ctx.beginPath();
+		ctx.arc(x, y, baseRadius, 0, Math.PI*2, false);
+		ctx.fill();
+
+		xAngle += xSpeed;
+		yAngle += ySpeed;
+
+		requestAnimationFrame(render);
+	}
+
+};
--- Scripts/trig2.js
+++ Scripts/trig2.js
@@ -4,65 +4,54 @@ window.onload = function() {
 	const width = canvas.width = window.innerWidth;
 	const height = canvas.height = window.innerHeight;
 
-	let centerY = height * 0.5,
-	    centerX = width * 0.5,
-	    baseRadius = 5,
-	    xRadius = 200,
-	    yRadius = 200,
-	    xAngle = 0.01,
-	    yAngle = 0.01,
-	    xSpeed = 0.02,
-	    ySpeed = 0.02,
-	    r = 0,
-	    g = 0,
-	    b = 0;
 
-	let counter = 0;
+	let arrowX = width/2;
+	let arrowY = height/2;
+	let dx, dy;
+	let angle = 0;
+
+	let mouse = {
+		x: 0,
+		y: 0
+	}
+
+	document.onmousemove = getMouseLocation;
+
+	function getMouseLocation(e) {
+		mouse.x = e.clientX;
+		mouse.y = e.clientY;
+	}
+
+	function updateAngle() {
+		dx = mouse.x - arrowX;
+		dy = mouse.y - arrowY;
+		angle = Math.atan2(dy,dx);
+	}
+
+	function drawArrow() {
+		ctx.save();
+		ctx.translate(arrowX, arrowY);
+		ctx.rotate(angle);
+
+		ctx.beginPath();
+		ctx.moveTo(20, 0);
+		ctx.lineTo(-20, 0);
+		ctx.moveTo(20, 0);
+		ctx.lineTo(10, -10);
+		ctx.moveTo(20, 0);
+		ctx.lineTo(10, 10);
+		ctx.stroke();
+
+		ctx.restore();
+	}
 
 	render();
 
-	document.onkeydown = getKey;
-
-	function getKey(e) {
-		switch (e.code) {
-			case "ArrowUp":
-				ySpeed += 0.01;
-				break;
-			case "ArrowDown":
-				ySpeed += -0.01;		
-				break; 
-			case "ArrowRight":
-				xSpeed += 0.01;
-				break;
-			case "ArrowLeft":
-				xSpeed += -0.01;
-				break;
-			default:
-				return;
-		}
-
-		yAngle = 0;
-		xAngle = 0;
-		ctx.clearRect(0,0,width,height);
-	}
-
 	function render() {
-		counter++;
-		let x = centerX + Math.cos(xAngle) * xRadius;
-		let y = centerY + Math.sin(yAngle) * yRadius;
-		
-		ctx.fillStyle = `rgb(${r},${g},${b})`;
+		ctx.clearRect(0, 0, width, height);
 
-		ctx.beginPath();
-		ctx.arc(x, y, baseRadius, 0, Math.PI*2, false);
-		ctx.fill();
-
-		ctx.fillStyle = "rgb(0,0,0)";
-		ctx.font = "40px Arial";
-		ctx.fillText("" + Math.round(xSpeed*100) + ":" + Math.round(ySpeed*100), 200, 100);
-
-		xAngle += xSpeed;
-		yAngle += ySpeed;
+		updateAngle();
+		drawArrow();
 
 		requestAnimationFrame(render);
 	}
