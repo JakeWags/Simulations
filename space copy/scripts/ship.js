1f771db5caba7b47a7f909a55a4893fc8a12095d
iMosher <iansmosher1@gmail.com>
2022-06-01 16:55:50 -0600

    Just Copying Space

---
+++ space copy/scripts/ship.js
@@ -0,0 +1,130 @@
+class Ship extends Particle {
+	#angle = 0;
+	shooting = false;
+	hasShot = false;
+	thrusting = false;
+	static shipLength = 30;
+	#bullets = [];
+	#points;
+
+	constructor(x = 50, y = 50, angle = 0, speed = 0) {
+		super(x,y,speed,angle);
+		this.#angle = angle;
+		this.#points = Ship.getAbsolutePoints(this.#angle);
+	}
+
+	getAngle() { return this.#angle; }
+	getLength() { return Ship.shipLength; }
+
+	// rotate the ship using radians
+	rotateRad(radians) {
+		this.#angle += radians;
+	}
+
+	// rotate the ship using degrees
+	rotateDeg(degrees) {
+		this.rotateRad(degrees*(Math.PI/180));
+	}
+		
+	// absolute coordinates relative to the base canvas
+	static getAbsolutePoints(angle, x, y) {
+		/*
+			dY = rsin(theta)
+			dX = r - rcos(theta)
+
+			polar to cartesian conversions
+		*/
+		return [
+			[x+Ship.shipLength*Math.cos(angle), y+Ship.shipLength*Math.sin(angle)],
+			[x+Ship.shipLength/2*Math.cos(angle+4*Math.PI/6),y+Ship.shipLength/2*Math.sin(angle+4*Math.PI/6)],
+			[x-Ship.shipLength/2*Math.cos(angle), y-Ship.shipLength/2*Math.sin(angle)],
+			[x+Ship.shipLength/2*Math.cos(angle-4*Math.PI/6),y+Ship.shipLength/2*Math.sin(angle-4*Math.PI/6)]
+			];
+	}
+
+	#drawFire(ctx) {
+		if(this.thrusting) {
+			ctx.beginPath();
+			ctx.moveTo(-Ship.shipLength/3, 0);
+			ctx.lineTo(-Ship.shipLength/3-Ship.shipLength,0);
+			ctx.strokeStyle = "red";
+			ctx.stroke();
+		}
+	}
+
+	// uses canvas translation and relative points
+	draw(ctx) {
+		// ctx.fillStyle = "white";
+		//let points = this.#computePoints();
+		
+		ctx.translate(this.getX(), this.getY());
+		ctx.rotate(this.#angle);
+		ctx.beginPath();
+
+		ctx.moveTo(Ship.shipLength, 0);
+		ctx.lineTo(-Ship.shipLength/2, -Ship.shipLength/3);
+		ctx.lineTo(-Ship.shipLength/3, 0);
+		ctx.lineTo(-Ship.shipLength/2, Ship.shipLength/3);
+		ctx.lineTo(Ship.shipLength, 0);
+		ctx.strokeStyle = "orange";
+		ctx.stroke();
+		ctx.fillStyle = "orange";
+		ctx.fill();
+
+
+		this.#drawFire(ctx);
+		
+
+		return ctx;
+	}
+
+	update(ctx) {
+		ctx.save();
+
+		ctx = this.draw(ctx);
+		this.shoot();
+
+		super.update();
+
+		ctx.restore();
+
+		this.#drawBullets(ctx);
+		this.#deleteBullets();
+	}
+
+	#computeBulletVelocity() {
+		// needs to account for the speed of the ship relative to the direction the bullet is travelling
+		// bullet shoots out at Bullet.bulletSpeed
+		let bVel = new Vector(0,0);
+		bVel.setLength(Bullet.bulletSpeed);
+		bVel.setAngle(this.#angle);
+
+		let sVel = super.getVelocity();
+		return sVel.add(bVel);
+	}
+
+	shoot() {
+		if (this.shooting && !this.hasShot) {
+			let bVel = this.#computeBulletVelocity();
+			this.#points = Ship.getAbsolutePoints(this.#angle, super.getX(), super.getY());
+			this.#bullets.push(new Bullet(this.#points[0][0], this.#points[0][1], bVel.getLength(), bVel.getAngle(), this.#angle));
+			this.hasShot = true;
+		}
+	}
+
+	#drawBullets(ctx) {
+		this.#bullets.forEach((e) => {
+			e.draw(ctx);
+		});
+	}
+
+	#deleteBullets() {
+		this.#bullets.forEach((e) => {
+			if (e.getX() > window.innerWidth || e.getX() < 0 || e.getY() < 0 || e.getY() > window.innerHeight) {
+				this.#bullets = this.#bullets.filter(b => b !== e); // filter the array for everything other than the bullet to delete
+
+			}
+		});
+	}
+
+}
---
+++ space copy/scripts/space.js
@@ -0,0 +1,133 @@
+window.onload = function() {
+	let canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
+	const width = canvas.width = window.innerWidth;
+	const height = canvas.height = window.innerHeight;
+
+	let ship = new Particle(width / 2, height / 2, 0, 0);
+	let thrust = new Vector(0,0);
+
+	let shipAngle = 0;
+
+	let thrusting = false,
+		turningLeft = false,
+		turningRight = false,
+		shooting = false;
+
+
+	let ship2 = new Ship(Math.floor(width/2), Math.floor(height/2), 0, 0);
+
+
+	const keydown = (e) => {
+		console.log(e.key);
+		switch (e.key) {
+			case "ArrowRight":
+				turningRight = true;
+				break;
+			case "ArrowLeft":
+				turningLeft = true;
+				break;
+			case "ArrowUp":
+				ship2.thrusting = true;
+				break;
+			case " ":
+				ship2.shooting = true;
+				break;
+		}
+	}
+
+	const keyup = (e) => {
+		switch (e.key) {
+			case "ArrowRight":
+				turningRight = false;
+				break;
+			case "ArrowLeft":
+				turningLeft = false;
+				break;
+			case "ArrowUp":
+				ship2.thrusting = false;
+				break;
+			case " ":
+				ship2.hasShot = false;
+				ship2.shooting = false;
+				break;
+		}
+	}
+
+	document.onkeydown = keydown;
+	document.onkeyup = keyup;
+
+
+	const edgeWrap = () => {
+		if (ship2.getPosition().getX() > width) { // right wrap to left
+			ship2.getPosition().setX(0);
+		}
+		if (ship2.getPosition().getX() < 0) { // left wrap to right
+			ship2.getPosition().setX(width);
+		}
+
+		if (ship2.getPosition().getY() > height) { // bottom wrap to top
+			ship2.getPosition().setY(0);
+		}
+		if (ship2.getPosition().getY() < 0) { // top wrap to bottom
+			ship2.getPosition().setY(height);
+		}
+	}
+
+	const drawShip = () => {
+		ctx.save();
+		ctx.translate(ship.getPosition().getX(), ship.getPosition().getY());
+		ctx.rotate(shipAngle);
+
+		ctx.beginPath();
+		ctx.moveTo(10, 0);
+		ctx.lineTo(-10, -7);
+		ctx.lineTo(-10, 7);
+		ctx.lineTo(10, 0);
+		ctx.fill();
+		ctx.stroke();
+
+		ctx.restore();
+	}
+
+	const rotate = () => {
+		if (turningRight) {
+			ship2.rotateRad(0.05);
+		}
+		if (turningLeft) {
+			ship2.rotateRad(-0.05);
+		}
+
+	}
+
+	const shoot = () => {
+		ship2.shoot();
+	}
+
+	const applyThrust = () => {
+		if (ship2.thrusting) {
+			thrust.setX(Math.cos(ship2.getAngle()) * 0.1);
+			thrust.setY(Math.sin(ship2.getAngle()) * 0.1);
+		} else {
+			thrust.setLength(0);
+		}
+	}
+
+	// let delta = Date.now();
+
+	const update = () => {
+		ctx.clearRect(0, 0, width, height);
+		// delta = Date.now() - delta;
+
+		rotate();
+		applyThrust();
+		ship2.accelerate(thrust);
+		ship2.update(ctx);
+
+		edgeWrap();
+
+		requestAnimationFrame(update);
+	}
+
+	update();
+}
